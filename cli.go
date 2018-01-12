1ecd3f4e4893c195bb5f8d5c3cd0f8d0b30c4e91
tnakata <f.seasons017@gmail.com>
2018-01-12 17:42:57 +0900

    Add debug mode

--- cli.go
+++ cli.go
@@ -14,6 +14,8 @@ const (
 	EnvGitHubAPI = "GITHUB_API"
 	// EnvGitHubToken is env var to set GitHub API token.
 	EnvGitHubToken = "GITHUB_TOKEN"
+	// EnvDebug is env var to handle debug mode.
+	EnvDebug = "GHRD_DEBUG"
 )
 
 // Exit code are in value that represent an exit code for a particular error.
@@ -38,15 +40,21 @@ type CLI struct {
 	outStream, errStream io.Writer
 }
 
+// Debugf prints debug output.
+func Debugf(format string, args ...interface{}) {
+	if env := os.Getenv(EnvDebug); len(env) > 0 {
+		log.Printf("[DEBUG]"+format+"\n", args...)
+	}
+}
+
 // Run invokes the CLI with the given arguments.
 func (cli *CLI) Run(args []string) int {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-
 	var (
 		owner    string
 		repo     string
 		token    string
 		filepath string
+		debug    bool
 	)
 
 	flags := flag.NewFlagSet("ghr-downloader", flag.ContinueOnError)
@@ -65,9 +73,17 @@ func (cli *CLI) Run(args []string) int {
 	flags.StringVar(&filepath, "path", "./", "")
 	flags.StringVar(&filepath, "p", "./", "")
 
+	flags.BoolVar(&debug, "debug", false, "")
+	flags.BoolVar(&debug, "d", false, "")
+
 	err := flags.Parse(args[1:])
 	if err != nil {
 		return ExitCodeParseFlagsError
+	}
+
+	if debug {
+		os.Setenv(EnvDebug, "1")
+		Debugf("Run as debug mode")
 	}
 
 	// Set base GitHub API. Base URL can be provided via env var, this is for GHE.
@@ -81,9 +97,14 @@ func (cli *CLI) Run(args []string) int {
 
 	gc := NewGitHubClient(owner, repo, token, baseURLStr)
 
+	Debugf("Owner: %s", owner)
+	Debugf("Repository: %s", repo)
+	Debugf("GitHub API URL: %s", baseURLStr)
+
 	var tag string
 	parsedArgs := flags.Args()
 	if len(parsedArgs) > 2 {
+		Debugf("Error: invalid arguments")
 		return ExitCodeInvalidArgs
 	} else if len(parsedArgs) == 1 {
 		tag = parsedArgs[0]
@@ -91,19 +112,25 @@ func (cli *CLI) Run(args []string) int {
 
 	tag, err = gc.GetTag(tag)
 	if err != nil {
+		Debugf("Error: %s", err)
 		return ExitCodeTagNotFound
 	}
+	Debugf("Tag: %s", tag)
 
 	id, name, err := gc.GetLatestAssetID(tag)
 	if err != nil {
+		Debugf("Error: %s", err)
 		return ExitCodeAssetIDNotFound
 	}
+	Debugf("Asset ID: %d", id)
 
 	file, err := os.OpenFile(fmt.Sprintf("%s/%s", filepath, name), os.O_WRONLY|os.O_CREATE, 0777)
 	if err != nil {
+		Debugf("Error: %s", err)
 		return ExitCodeOpenFileError
 	}
 	if err = gc.GetAsset(id, file); err != nil {
+		Debugf("Error: %s", err)
 		return ExitCodeAssetNotFound
 	}
 
